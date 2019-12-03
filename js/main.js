ee3eaf7abf1f76b033e52a55af1a7d5518f8537f
ngovangioi <gioinvebisolvn@gmail.com>
2019-12-03 17:43:24 +0700

    s

--- js/main.js
+++ js/main.js
@@ -1,60 +1,21 @@
 $(document).ready(function() {
     $('a').smoothScroll();
 
-    $('.slick-track').slick({
-        dots: true,
-    });
+    
 
-    $('button.slick-next').click(function(e) {
-        $(".slick-track").slick('slickNext');
-    });
-    $('button.slick-prev').click(function() {
-        $(".slick-track").slick('slickPrev');
-    });
-
-    $('.slick-dots>li').click(function(e) {
-
-        e.preventDefault();
-
-        //disable all active
-        $(".slick-dots>li>img").each(function() {
-            var src = $(this).attr("src")
-            $(this).attr("src", src.replace('-active', ''));
-        });
-
-        //active
-        var childSrc = $(this).children('img')
-        childSrc.attr('src', childSrc.attr('src').replace('.png', '-active.png'))
-
-        var slideno = $(this).data('slide');
-        $('.slick-track').slick('slickGoTo', slideno - 1);
+   
 
 
-    });
-
-    //change active when click next, prev
-    $('.slick-track').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
-        var CurrentSlideDom = $(slick.$slides.get(currentSlide));
-        var dataId = $(slick.$slides[nextSlide]).data('index');
-
-        /* var list = $(".slick-dots>li>img")
-        list.each(function() {
-            var src = $(this).attr("src")
-            $(this).attr("src", src.replace('-active', ''));
-        });
-
-        //active
-        var childSrc = $(list[dataId]);
-        console.log(dataId);
-        childSrc.attr('src', childSrc.attr('src').replace('.png', '-active.png')) */
-
+    $('.student-feedback').slick({
+        autoplay:true,
+        arrows:true
     });
 });
 
 function makeTimer() {
 
     //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");	
-    var endTime = new Date("01 December 2019 9:56:00 GMT+01:00");
+    var endTime = new Date("19 December 2019 0:00:00 GMT+01:00");
     endTime = (Date.parse(endTime) / 1000);
 
     var now = new Date();
