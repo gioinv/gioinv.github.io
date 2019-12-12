410ce7041f1c4972dfe831762a5707930b92b92f
ngovangioi <gioinvebisolvn@gmail.com>
2019-12-12 11:30:54 +0700

    dd

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+  var errorMsg = 'Không thể gửi thông tin!\n Vui lòng lien lạc ban quản trị để biết thông tin chi tiết.';
+
   $('a').smoothScroll();
 
   $('.student-feedback').slick({
@@ -6,8 +8,80 @@ $(document).ready(function () {
     arrows: true
   });
 
-  
-  
+  $("form#tuvan").on("submit", function (e) {
+    e.preventDefault();
+    $('#spinTv').show()
+    $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbzctdc_JfS_0IYKpYzBvS9NlHWeHWg6jHGHR08s-NoTE-2QWsU9/exec",
+      type: "POST",
+      data: {
+        hoten: $('#hotenTv').val(),
+        sdt: '\'' + $('#sdtTv').val(),
+        cauhoi: $('#cauhoiTv').val()
+      },
+      contentType: "application/javascript",
+      dataType: 'jsonp'
+    })
+      .done(function (res) {
+        console.log('success')
+        $('#spinTv').hide()
+      })
+      .fail(function (e) {
+        console.log("error")
+        $('#spinTv').hide()
+      });
+
+    window.receipt = function (res) {
+      if (res.success) {
+        $('#successModal').modal('show');
+        $("form#tuvan").trigger("reset");
+      } else {
+        alert(errorMsg);
+      }
+
+      $('#spinTv').hide()
+    }
+  });
+
+  $("form#dangky").on("submit", function (e) {
+    e.preventDefault();
+    $('#spinDk').show()
+    
+    $.ajax({
+      url: "https://script.google.com/macros/s/AKfycbzRGoHL1kOxkVbdfvuhMNI_06eZI1dSlpJ-Ti9XpJB7XUJH5aU/exec",
+      type: "POST",
+      data: {
+        anhchi: $('#anhchi').val(),
+        hoten: $('#hoten').val(),
+        sdt: '\'' + $('#sdt').val(),
+        email: $('#email').val()
+      },
+      contentType: "application/javascript",
+      dataType: 'jsonp'
+    })
+      .done(function (res) {
+        console.log('success')
+        $('#spinDk').hide()
+      })
+      .fail(function (e) {
+        console.log("error")
+        $('#spinDk').hide()
+      });
+
+    window.receipt = function (res) {
+      
+      if (res.success) {
+        $('#successModal').modal('show');
+        $("form#dangky").trigger("reset");
+      } else {
+        alert(errorMsg);
+        $('#spinDk').hide()
+      }
+
+      $('#spinDk').hide()
+    }
+  });
+
 });
 
 function makeTimer() {
